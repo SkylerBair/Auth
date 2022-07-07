3a6d26e12b76d233ea4be74a43b2ea99b36e6ba6
skyler bair <skylertimothybair@hotmail.com>
2022-07-07 16:39:39 -0600

    completed auth project

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/SkylerBair/Auth
 
 go 1.17
+
+require golang.org/x/crypto v0.0.0-20220622213112-05595931fe9d
--- main.go
+++ main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"bytes"
+	"crypto/sha256"
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+	"time"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
 const hashCost = 8
@@ -19,11 +25,56 @@ type Credentials struct {
 	Username string `json:"username"`
 }
 
+var usersMap map[string][]byte = make(map[string][]byte)
+
 func Signup(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("testing"))
+	creds := &Credentials{}
+	err := json.NewDecoder(r.Body).Decode(creds) //
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	// Salt and hash the password using the bcrypt algorithm
+	// The second argument is the cost of hashing, which we arbitrarily set as 8 (this value can be more or less, depending on the computing power you wish to utilize)
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(creds.Password), 8)
+
+	usersMap[creds.Username] = hashedPassword
+
+	// create a empty map to store the usernames and passwords in using the username as the key and the hashed password as the value.
+	w.Write(hashedPassword)
 	fmt.Println(w)
 }
 
 func Signin(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("signin bitch")
+	creds := &Credentials{}
+	err := json.NewDecoder(r.Body).Decode(creds) //
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	//ugly to many ifs needs to be refactored into smaller functions
+	if hashed, ok := usersMap[creds.Username]; ok {
+		// compare hash and
+		if err = bcrypt.CompareHashAndPassword(hashed, []byte(creds.Password)); err != nil {
+
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		} else {
+			now := time.Now()
+			token := bytes.NewBuffer([]byte(now.String()))
+			h := sha256.New()
+			_, err := h.Write(token.Bytes())
+			if err != nil {
+				w.WriteHeader(http.StatusBadGateway)
+				return
+			}
+			b := h.Sum(nil)
+			w.Write(b)
+			return
+		}
+
+	} else {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
 }
