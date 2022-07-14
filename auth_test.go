1b1aa3a78d4cd1d047e1934058f21436cc74703e
skyler bair <skylertimothybair@hotmail.com>
2022-07-14 11:18:52 -0600

    completed signup test and got it passing

--- auth_test.go
+++ auth_test.go
@@ -1,7 +1,24 @@
 package main
 
-import "testing"
+import (
+	"bytes"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
 
 func TestSignup(t *testing.T) {
+	b := []byte(`{"username":"foo","password":"bar"}`)
+	payload := bytes.NewReader(b)
+	req, err := http.NewRequest(http.MethodGet, "/signup", payload)
+	if err != nil {
+		t.Fail()
+	}
+	res := httptest.NewRecorder()
 
+	Signup(res, req)
+	got := res.Body.String()
+	if len(got) == 0 {
+		t.Errorf("expected token")
+	}
 }
